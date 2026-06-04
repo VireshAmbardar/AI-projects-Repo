27e53738a3a7842265ce5e0201e257984b2c0839
Viresh Ambardar <ambardarviresh18@gmail.com>
2026-06-04 04:05:32 +0000

    uv implimentation

---
+++ GoogleADK/Module1-Intro/my_first_agent/agent.py
@@ -0,0 +1,32 @@
+import os
+from google.adk.agents.llm_agent import Agent
+import os
+from dotenv import load_dotenv  
+load_dotenv()
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY environment variable is not set.")
+
+# # os.environ['GOOGLE_API_KEY'] = "[GCP_API_KEY]"
+# root_agent = Agent(
+#     model='gemini-2.5-flash',
+#     name='root_agent',
+#     description='A helpful assistant for user questions.',
+#     instruction='Answer user questions to the best of your knowledge',
+# )
+
+# from google.adk.agents.llm_agent import Agent
+
+# Mock tool implementation
+def get_current_time(city: str) -> dict:
+    """Returns the current time in a specified city."""
+    return {"status": "success", "city": city, "time": "10:30 AM"}
+
+root_agent = Agent(
+    model='gemini-flash-latest',
+    name='root_agent',
+    description="Tells the current time in a specified city.",
+    instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
+    tools=[get_current_time],
+)
